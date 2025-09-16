cb28aded1d1e462cdbcd8aca56b1cbcb1376d0aa
Mayank Goswami <mayankhacker04@gmail.com>
2025-09-16 16:49:30 +0530

    added functionality to upload data in form of csv file

--- work.py
+++ work.py
@@ -17,6 +17,10 @@ if "df" not in st.session_state:
 
 with data_tabs:
     st.write("Current DataFrame:")
+    uploaded_csv = st.file_uploader("Upload Data", type="csv")
+    if uploaded_csv is not None:
+        dataframe = pd.read_csv(uploaded_csv)
+        st.session_state.df = dataframe
     df = st.data_editor(st.session_state.df, num_rows="dynamic")
 
     if st.button("Add Column"):
@@ -33,17 +37,45 @@ with data_tabs:
         st.session_state.df = df
 
 with plot_tabs:
+    plot = st.selectbox("Select which type of plot", options = ("Line Plot", "Scatter Plot", "Bar Plot"))
+    if "plot" not in st.session_state:
+        st.session_state.plot = None 
+    st.session_state.plot = plot 
     st.write("Plotting")
 
-    if not st.session_state.df.empty:
-        x_axis = st.selectbox("Select X-axis", options=st.session_state.df.columns, key="x_axis")
-        y_axis = st.selectbox("Select Y-axis", options=st.session_state.df.columns, key="y_axis")
-
-        if x_axis and y_axis:
-            fig = px.line(st.session_state.df, x=x_axis, y=y_axis, title="Line Plot")
-            with st.container(height=500, border=False):
-                st.plotly_chart(fig, use_container_width=True)
+    if st.session_state.plot == "Line Plot":
+        if not st.session_state.df.empty:
+            x_axis = st.selectbox("Select X-axis", options=st.session_state.df.columns, key="x_axis")
+            y_axis = st.selectbox("Select Y-axis", options=st.session_state.df.columns, key="y_axis")
+            if x_axis and y_axis:
+                fig = px.line(st.session_state.df, x=x_axis, y=y_axis, title="Line Plot")
+                with st.container(height=500, border=False):
+                    st.plotly_chart(fig, use_container_width=True)
+            else:
+                st.write("Please select both X and Y axes to plot.")
         else:
-            st.write("Please select both X and Y axes to plot.")
-    else:
-        st.write("DataFrame is empty. Please add data to plot.")
+            st.write("DataFrame is empty. Please add data to plot.")
+    elif st.session_state.plot == "Bar Plot":
+        if not st.session_state.df.empty:
+            x_axis = st.selectbox("Select X-axis", options=st.session_state.df.columns, key="x_axis")
+            y_axis = st.selectbox("Select Y-axis", options=st.session_state.df.columns, key="y_axis")
+            if x_axis and y_axis:
+                fig = px.bar(st.session_state.df, x=x_axis, y=y_axis, title="Bar Plot")
+                with st.container(height=500, border=False):
+                    st.plotly_chart(fig, use_container_width=True)
+            else:
+                st.write("Please select both X and Y axes to plot.")
+        else:
+            st.write("DataFrame is empty. Please add data to plot.")
+    elif st.session_state.plot == "Scatter Plot":
+        if not st.session_state.df.empty:
+            x_axis = st.selectbox("Select X-axis", options=st.session_state.df.columns, key="x_axis")
+            y_axis = st.selectbox("Select Y-axis", options=st.session_state.df.columns, key="y_axis")
+            if x_axis and y_axis:
+                fig = px.scatter(st.session_state.df, x=x_axis, y=y_axis, title="Line Plot")
+                with st.container(height=500, border=False):
+                    st.plotly_chart(fig, use_container_width=True)
+            else:
+                st.write("Please select both X and Y axes to plot.")
+        else:
+            st.write("DataFrame is empty. Please add data to plot.")
