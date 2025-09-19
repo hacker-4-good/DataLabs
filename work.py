fbaa2c297596939b663bec050bddfb662e28a257
Mayank Goswami <mayankhacker04@gmail.com>
2025-09-19 16:53:43 +0530

    Added Edit details (axes data) on Plot

--- work.py
+++ work.py
@@ -24,7 +24,7 @@ with data_tabs:
     df = st.data_editor(st.session_state.df, num_rows="dynamic")
 
     if st.button("Add Column"):
-        new_column_name = f"h({chr(65 + len(st.session_state.df.columns))})" # Generate column names like h(C), h(D), etc.
+        new_column_name = f"h({chr(65 + len(st.session_state.df.columns))})"
         st.session_state.df[new_column_name] = None
         st.rerun()
 
@@ -40,6 +40,16 @@ with plot_tabs:
     plot = st.selectbox("Select which type of plot", options = ("Line Plot", "Scatter Plot", "Bar Plot"))
     if "plot" not in st.session_state:
         st.session_state.plot = None 
+    if "axes" not in st.session_state:
+        st.session_state.axes = None
+    @st.dialog("Change the axis data")
+    def change_data():
+        st.write("Mention the x-axis and y-axis name")
+        x_axis = st.text_input(label="Text to display on X-axis")
+        y_axis = st.text_input(label="Text to display on Y-axis")
+        if st.button("Make Changes"):
+            st.session_state.axes = {"x_axis": x_axis, "y_axis": y_axis}
+            st.rerun()
     st.session_state.plot = plot 
     st.write("Plotting")
 
@@ -48,7 +58,10 @@ with plot_tabs:
             x_axis = st.selectbox("Select X-axis", options=st.session_state.df.columns, key="x_axis")
             y_axis = st.selectbox("Select Y-axis", options=st.session_state.df.columns, key="y_axis")
             if x_axis and y_axis:
-                fig = px.line(st.session_state.df, x=x_axis, y=y_axis, title="Line Plot")
+                if st.session_state.axes == None:
+                    fig = px.line(st.session_state.df, x=x_axis, y=y_axis, title="Line Plot")
+                else:
+                    fig = px.line(st.session_state.df, x=x_axis, y=y_axis, title="Line Plot").update_layout(xaxis_title=st.session_state.axes['x_axis'], yaxis_title=st.session_state.axes['y_axis'])
                 with st.container(height=500, border=False):
                     st.plotly_chart(fig, use_container_width=True)
             else:
@@ -60,7 +73,10 @@ with plot_tabs:
             x_axis = st.selectbox("Select X-axis", options=st.session_state.df.columns, key="x_axis")
             y_axis = st.selectbox("Select Y-axis", options=st.session_state.df.columns, key="y_axis")
             if x_axis and y_axis:
-                fig = px.bar(st.session_state.df, x=x_axis, y=y_axis, title="Bar Plot")
+                if st.session_state.axes == None:
+                    fig = px.bar(st.session_state.df, x=x_axis, y=y_axis, title="Bar Plot")
+                else:
+                    fig = px.bar(st.session_state.df, x=x_axis, y=y_axis, title="Bar Plot").update_layout(xaxis_title=st.session_state.axes['x_axis'], yaxis_title=st.session_state.axes['y_axis'])
                 with st.container(height=500, border=False):
                     st.plotly_chart(fig, use_container_width=True)
             else:
@@ -72,10 +88,15 @@ with plot_tabs:
             x_axis = st.selectbox("Select X-axis", options=st.session_state.df.columns, key="x_axis")
             y_axis = st.selectbox("Select Y-axis", options=st.session_state.df.columns, key="y_axis")
             if x_axis and y_axis:
-                fig = px.scatter(st.session_state.df, x=x_axis, y=y_axis, title="Line Plot")
+                if st.session_state.axes == None:
+                    fig = px.scatter(st.session_state.df, x=x_axis, y=y_axis, title="Line Plot")
+                else:
+                    fig = px.scatter(st.session_state.df, x=x_axis, y=y_axis, title="Line Plot").update_layout(xaxis_title=st.session_state.axes['x_axis'], yaxis_title=st.session_state.axes['y_axis'])
                 with st.container(height=500, border=False):
                     st.plotly_chart(fig, use_container_width=True)
             else:
                 st.write("Please select both X and Y axes to plot.")
         else:
             st.write("DataFrame is empty. Please add data to plot.")
+    if st.button("Edit Details"):
+        change_data()
