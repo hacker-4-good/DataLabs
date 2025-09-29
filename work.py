c4f39922257a7644538b263626650bd5f9e34bf0
Mayank Goswami <91790864+hacker-4-good@users.noreply.github.com>
2025-09-29 16:24:03 +0530

    Refactor plotting functions to use modular plots

--- work.py
+++ work.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+from plots.basic_charts.line_plot import line_plot
+from plots.basic_charts.bar_plot import bar_plot
+from plots.basic_charts.scatter_plot import scatter_plot
+from agent import call_agent_bot
 
 st.set_page_config(page_title="DataLabs Visualization Tool", layout="wide")
 
@@ -42,6 +45,8 @@ with plot_tabs:
         st.session_state.plot = None 
     if "axes" not in st.session_state:
         st.session_state.axes = None
+    if "figure" not in st.session_state:
+        st.session_state.figure = None
     @st.dialog("Change the axis data")
     def change_data():
         st.write("Mention the x-axis and y-axis name")
@@ -55,48 +60,19 @@ with plot_tabs:
 
     if st.session_state.plot == "Line Plot":
         if not st.session_state.df.empty:
-            x_axis = st.selectbox("Select X-axis", options=st.session_state.df.columns, key="x_axis")
-            y_axis = st.selectbox("Select Y-axis", options=st.session_state.df.columns, key="y_axis")
-            if x_axis and y_axis:
-                if st.session_state.axes == None:
-                    fig = px.line(st.session_state.df, x=x_axis, y=y_axis, title="Line Plot")
-                else:
-                    fig = px.line(st.session_state.df, x=x_axis, y=y_axis, title="Line Plot").update_layout(xaxis_title=st.session_state.axes['x_axis'], yaxis_title=st.session_state.axes['y_axis'])
-                with st.container(height=500, border=False):
-                    st.plotly_chart(fig, use_container_width=True)
-            else:
-                st.write("Please select both X and Y axes to plot.")
+            line_plot()
         else:
             st.write("DataFrame is empty. Please add data to plot.")
     elif st.session_state.plot == "Bar Plot":
         if not st.session_state.df.empty:
-            x_axis = st.selectbox("Select X-axis", options=st.session_state.df.columns, key="x_axis")
-            y_axis = st.selectbox("Select Y-axis", options=st.session_state.df.columns, key="y_axis")
-            if x_axis and y_axis:
-                if st.session_state.axes == None:
-                    fig = px.bar(st.session_state.df, x=x_axis, y=y_axis, title="Bar Plot")
-                else:
-                    fig = px.bar(st.session_state.df, x=x_axis, y=y_axis, title="Bar Plot").update_layout(xaxis_title=st.session_state.axes['x_axis'], yaxis_title=st.session_state.axes['y_axis'])
-                with st.container(height=500, border=False):
-                    st.plotly_chart(fig, use_container_width=True)
-            else:
-                st.write("Please select both X and Y axes to plot.")
+            bar_plot()
         else:
             st.write("DataFrame is empty. Please add data to plot.")
     elif st.session_state.plot == "Scatter Plot":
         if not st.session_state.df.empty:
-            x_axis = st.selectbox("Select X-axis", options=st.session_state.df.columns, key="x_axis")
-            y_axis = st.selectbox("Select Y-axis", options=st.session_state.df.columns, key="y_axis")
-            if x_axis and y_axis:
-                if st.session_state.axes == None:
-                    fig = px.scatter(st.session_state.df, x=x_axis, y=y_axis, title="Line Plot")
-                else:
-                    fig = px.scatter(st.session_state.df, x=x_axis, y=y_axis, title="Line Plot").update_layout(xaxis_title=st.session_state.axes['x_axis'], yaxis_title=st.session_state.axes['y_axis'])
-                with st.container(height=500, border=False):
-                    st.plotly_chart(fig, use_container_width=True)
-            else:
-                st.write("Please select both X and Y axes to plot.")
+            scatter_plot()
         else:
             st.write("DataFrame is empty. Please add data to plot.")
+    call_agent_bot()
     if st.button("Edit Details"):
         change_data()
